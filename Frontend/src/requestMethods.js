7aeb8cef49c02ab76a05a4ecf04284d062bba49a
unknown <jameskagunga2022@gmail.com>
2024-01-13 13:39:28 -0800

    added changes

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL="http://localhost:8800/api/v1/";
+const BASE_URL="https://api.aimtasker.com/api/v1/";
 const BASE_URL_1="http://localhost:8000/api/v1/";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGI2NzM2ZjJmZDM2YTBlMjA5YWE3MSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDQ5MTQ1MDUsImV4cCI6MTY0NTE3MzcwNX0.TRfdqYrZXhFrj7kYmpv1jnQtYMxMPu-unP-g9cYux4Q";
 
